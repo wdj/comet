573e525fc40b05406729b64839984143697d30cd
Wayne Joubert <joubert@ornl.gov>
2015-10-28 17:11:21 -0400

    Added timing results with Czek 2-way all2all GPU.

--- scripts/run.sh
+++ scripts/run.sh
@@ -16,15 +16,14 @@
 ACCOUNT=stf006
 
 WD=$PWD
-EXEC=$WD/../install_debug/bin/genomics_metric
+#EXEC=$WD/../install_debug/bin/genomics_metric
+EXEC=$WD/genomics_metric
 
 #---cd to Lustre to be able to aprun on titan.
 pushd $MEMBERWORK/$ACCOUNT > /dev/null
 
-for all2all in yes no; do
-
 #for compute_method in CPU GPU ; do
-for compute_method in CPU GPU ; do
+for compute_method in CPU ; do
 
   #echo \
   #aprun -n1 $EXEC \
@@ -51,17 +50,46 @@ for compute_method in CPU GPU ; do
   #aprun -n1 -N1 $EXEC \
   #    --num_field 1 --num_vector_local 4 --all2all yes \
   #    --compute_method $compute_method --verbosity 2
-  for n in 1 3 ; do
-  aprun -n$n -N1 $EXEC \
-      --num_field 10 --num_vector_local 24 --all2all $all2all \
-      --num_field 10 --num_vector_local $(( 24 / $n )) --all2all $all2all  \
-      --compute_method $compute_method --verbosity 1
+
+
+  #for nproc in {1..4} ; do
+  #  num_vector_local=$(( 6000 / $nproc ))
+  #  aprun -n$nproc -N1 $EXEC \
+  #    --num_field 500 --num_vector_local $num_vector_local --all2all yes \
+  #    --compute_method $compute_method --verbosity 1
+  #done
+
+#  for log2_num_vector_local in 14 13 12 11 ; do
+#    num_vector_local=$(( 1 << $log2_num_vector_local ))
+#  for log2_num_field in {11..11} ; do
+#    num_field=$(( 1 << $log2_num_field ))
+#
+#    log2_nproc=$(( 2 * ( 14 - $log2_num_vector_local ) ))
+#    nproc=$(( 1 << $log2_nproc ))
+#
+#    echo \
+#    time aprun -n$nproc -N1 $EXEC \
+#      --num_field $num_field --num_vector_local $num_vector_local \
+#      --all2all yes --compute_method $compute_method --verbosity 1
+#    time aprun -n$nproc -N1 $EXEC \
+#      --num_field $num_field --num_vector_local $num_vector_local \
+#      --all2all yes --compute_method $compute_method --verbosity 1
+#  done
+#  done
+
+  for nproc_sqrt in 1 2 4 8  ; do
+    nproc=$(( $nproc_sqrt * $nproc_sqrt ))
+    num_field=3072
+    num_field=30
+    num_vector_local=$(( 16384 / $nproc_sqrt ))
+    time aprun -n$nproc -N1 $EXEC \
+      --num_field $num_field --num_vector_local $num_vector_local \
+      --all2all yes --compute_method $compute_method --verbosity 1
   done
+
   #aprun -n4 -N1 $EXEC \
   #    --num_field 1 --num_vector_local 1 --all2all yes \
   #    --compute_method $compute_method --verbosity 2
-
-done
 
 done
 
