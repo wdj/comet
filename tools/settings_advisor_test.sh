f69e5cdff513f50e36081ea65913020b8a41f327
Wayne Joubert <joubert@ornl.gov>
2018-02-20 09:23:46 -0500

    Work on settings advisor.

---
+++ tools/settings_advisor_test.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#------------------------------------------------------------------------------
+
+./settings_advisor --help
+
+./settings_advisor --num_node 6000 --platform Titan \
+                   --num_vector 28342758 --num_field 882 \
+                   --metric_type ccc --num_way 2
+
+./settings_advisor --num_node 6000 --platform Titan \
+                   --num_vector 28342758 --num_field 882 \
+                   --metric_type czekanowski --num_way 2
+
+
+
+
+
+
+
+
+#------------------------------------------------------------------------------
