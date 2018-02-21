158f342ef7596bac44998b5d650a344bb72bbec0
Wayne Joubert <joubert@ornl.gov>
2018-02-20 20:20:57 -0500

    Debugged memory computation for 3-way, settings advisor.

--- tools/settings_advisor_test.sh
+++ tools/settings_advisor_test.sh
@@ -11,6 +11,11 @@
                    --num_vector 28342758 --num_field 882 \
                    --metric_type czekanowski --num_way 2
 
+./settings_advisor --num_node 3 --platform Titan \
+                   --num_vector 450 --num_field 1000 \
+                   --metric_type czekanowski --num_way 3 \
+                   --sparse no
+
 
 
 
